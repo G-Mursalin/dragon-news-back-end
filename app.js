6f6ce07557b3e6bc77cf6e0f55d57df5adb3d367
Golam Mursalin <golammursalin309@gmail.com>
2023-02-02 16:19:40 +0600

    multer added for image upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 const app = express();
 const categoriesRoute = require("./routes/categoriesRoute");
 const newsRoute = require("./routes/newsRoute");
@@ -10,6 +12,37 @@ const bookmarksRoute = require("./routes/bookmarkRoute");
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/image", express.static("image"));
+
+// Image Upload
+let imageName = "";
+const storage = multer.diskStorage({
+  destination: path.join("./image"),
+  filename: function (req, file, cb) {
+    imageName = Date.now() + path.extname(file.originalname);
+    cb(null, imageName);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+}).single("image");
+
+app.post("/upload-image", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(201).send({
+        success: false,
+      });
+    } else {
+      return res.status(201).send({
+        success: true,
+        url: "http://localhost:5000/image/" + imageName,
+      });
+    }
+  });
+});
 
 //Routs
 app.use("/api/v1/categories", categoriesRoute);
